1c34e7ab5542970a8baad629f12369752efbfbe6
PriyankaP1208 <ppriyapatil1208@gmail.com>
2020-12-23 22:54:27 +0530

    Added:Edit Existing information

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,12 +1,15 @@
 package FellowshipAddressBook;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-
 public class AddressBookMain 
 {
 	private static final String LINE_SEPARATOR = null;
@@ -16,7 +19,7 @@ public class AddressBookMain
 		while(choice<8)
         {
         	
-        		System.out.println("1.Create Adress Book 2.Add new person");
+        		System.out.println("1.Create Adress Book 2.Add new person 3.Edit");
         		System.out.println("Enter your choice:");
         		Scanner s1=new Scanner(System.in);
         		choice=s1.nextInt();
@@ -27,6 +30,9 @@ public class AddressBookMain
                         	break;
             		case 2:
                 			Add();
+                			break;
+            		case 3:
+                			Edit();
                 			break;
             	}
         }
@@ -60,5 +66,52 @@ public class AddressBookMain
         {
         	Person p1=new Person();
     		p1.info();
+        }
+        public static void Edit() throws IOException
+        {
+        	List<String> lines = new ArrayList<String>(); 
+        	String line = null;
+        	String address,address1,city,city1,state,state1;
+        	try
+        	{
+        		
+        		File f1 = new File("info.txt");
+        		FileReader fr = new FileReader(f1);
+        		BufferedReader br = new BufferedReader(fr);
+        		Scanner sc=new Scanner(System.in);
+        		System.out.println("Enter address:");
+                address=sc.nextLine();
+        		System.out.println("Enter address for replace:");
+        		address1=sc.nextLine();
+        		while ((line = br.readLine()) != null) 
+        		{
+        			if (line.contains(address))
+        				line = line.replace(address, address1);
+        			lines.add(line);
+        		}
+        		System.out.println("Enter city:");
+                city=sc.nextLine();
+        		System.out.println("Enter city for replace:");
+        		city1=sc.nextLine();
+        		while ((line = br.readLine()) != null) 
+        		{
+        			if (line.contains(address))
+        				line = line.replace(city, city1);
+        			lines.add(line);
+        		}
+        		fr.close();
+        		br.close();
+        		FileWriter fw = new FileWriter(f1);
+        		BufferedWriter out = new BufferedWriter(fw);
+        		for(String s : lines)
+        			out.write(s);
+        		out.flush();
+        		out.close();
+        	}
+        	catch (Exception ex) 
+        	{
+                ex.printStackTrace();
+            }
+        	
         }
 }
